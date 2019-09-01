0037df4dad0a00659f76271654db8faa54261647
Billy Huynh <billyhuynh@live.com>
2019-09-01 21:35:22 +1000

    Updates

---
+++ precache-manifest.bbd250f7a7493efe3382788c1ee1c121.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79298cd0d9debe300b94b4fcea072990",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "24c25d6eaa12a6be4d89",
+    "url": "/myportfolio/static/css/2.fda30995.chunk.css"
+  },
+  {
+    "revision": "517027ec75c035473578",
+    "url": "/myportfolio/static/css/main.e4e3b16c.chunk.css"
+  },
+  {
+    "revision": "24c25d6eaa12a6be4d89",
+    "url": "/myportfolio/static/js/2.91afe059.chunk.js"
+  },
+  {
+    "revision": "517027ec75c035473578",
+    "url": "/myportfolio/static/js/main.651060dc.chunk.js"
+  },
+  {
+    "revision": "edf24e10470540f155b3",
+    "url": "/myportfolio/static/js/runtime~main.b6d921c7.js"
+  },
+  {
+    "revision": "f51efffd7eeea9422e2ba0ad0305a161",
+    "url": "/myportfolio/static/media/avatar.f51efffd.PNG"
+  },
+  {
+    "revision": "009d099eb5d09cfd6f1af6d040209743",
+    "url": "/myportfolio/static/media/background.009d099e.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/myportfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/myportfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/myportfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/myportfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/myportfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/myportfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/myportfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/myportfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/myportfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/myportfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/myportfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/myportfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/myportfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/myportfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/myportfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/myportfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "63337cc73a290619340053d734f3b53e",
+    "url": "/myportfolio/static/media/overlay.63337cc7.png"
+  },
+  {
+    "revision": "318f23486867dfa7b0737c2c5e7c27b2",
+    "url": "/myportfolio/static/media/tetris.318f2348.PNG"
+  },
+  {
+    "revision": "48927f63b958bcd7bdde64d7e218bd1e",
+    "url": "/myportfolio/static/media/tictactoe.48927f63.PNG"
+  }
+]);
